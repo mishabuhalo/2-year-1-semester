00cdd0a237791523dd8d87b9d99d1fa13bcd4791
Misha Bukhalo <bukhalo1999@gmai.com>
2018-11-05 19:43:54 +0200

    Note Application version 1.0

---
+++ Lab_2/Lab_2/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Lab_2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.currNotes = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnNew = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnSave = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnOpen = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnDelete = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnAddArchive = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnShow = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator6 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnReturn = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.currNotes)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(59, 33);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(200, 20);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(59, 84);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(200, 203);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // currNotes
+            // 
+            this.currNotes.AllowUserToAddRows = false;
+            this.currNotes.AllowUserToDeleteRows = false;
+            this.currNotes.AllowUserToResizeColumns = false;
+            this.currNotes.AllowUserToResizeRows = false;
+            this.currNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.currNotes.Location = new System.Drawing.Point(286, 84);
+            this.currNotes.Name = "currNotes";
+            this.currNotes.ReadOnly = true;
+            this.currNotes.RowHeadersVisible = false;
+            this.currNotes.Size = new System.Drawing.Size(290, 203);
+            this.currNotes.TabIndex = 3;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNew,
+            this.toolStripSeparator1,
+            this.btnSave,
+            this.toolStripSeparator2,
+            this.btnOpen,
+            this.toolStripSeparator3,
+            this.btnDelete,
+            this.toolStripSeparator4,
+            this.btnAddArchive,
+            this.toolStripSeparator5,
+            this.btnShow,
+            this.toolStripSeparator6,
+            this.btnReturn});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(636, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnNew
+            // 
+            this.btnNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnNew.Image = ((System.Drawing.Image)(resources.GetObject("btnNew.Image")));
+            this.btnNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(35, 22);
+            this.btnNew.Text = "New";
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnSave
+            // 
+            this.btnSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(35, 22);
+            this.btnSave.Text = "Save";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnOpen.Image = ((System.Drawing.Image)(resources.GetObject("btnOpen.Image")));
+            this.btnOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(40, 22);
+            this.btnOpen.Text = "Open";
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
+            this.btnDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(44, 22);
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnAddArchive
+            // 
+            this.btnAddArchive.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnAddArchive.Image = ((System.Drawing.Image)(resources.GetObject("btnAddArchive.Image")));
+            this.btnAddArchive.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAddArchive.Name = "btnAddArchive";
+            this.btnAddArchive.Size = new System.Drawing.Size(88, 22);
+            this.btnAddArchive.Text = "Add to archive";
+            this.btnAddArchive.Click += new System.EventHandler(this.btnAddArchive_Click);
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnShow
+            // 
+            this.btnShow.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnShow.Image = ((System.Drawing.Image)(resources.GetObject("btnShow.Image")));
+            this.btnShow.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(83, 22);
+            this.btnShow.Text = "Show Archive";
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // toolStripSeparator6
+            // 
+            this.toolStripSeparator6.Name = "toolStripSeparator6";
+            this.toolStripSeparator6.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnReturn.Image = ((System.Drawing.Image)(resources.GetObject("btnReturn.Image")));
+            this.btnReturn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(133, 22);
+            this.btnReturn.Text = "Return to current notes";
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 312);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.currNotes);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Notes Application";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.currNotes)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.DataGridView currNotes;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnNew;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnSave;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton btnOpen;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton btnDelete;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton btnAddArchive;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripButton btnShow;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator6;
+        private System.Windows.Forms.ToolStripButton btnReturn;
+    }
+}
+
---
+++ Lab_2/Lab_2/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab_2
+{
+    public partial class MainForm : Form
+    {
+        DataTable currTable;
+        DataTable archTable;
+        DataSet currSet;
+        DataSet archSet;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            currSet = new DataSet();
+            currTable = new DataTable();
+            archTable = new DataTable();
+            archSet = new DataSet();
+
+           
+            currSet.ReadXml("Current.xml");
+            currSet.Tables.Add(currTable);
+            
+            currTable = currSet.Tables[0];
+
+            archSet.ReadXml("Archive.xml");
+            archSet.Tables.Add(archTable);
+
+            archTable = archSet.Tables[0];
+
+            if (currTable.Columns.Count <= 0)
+            {
+                currTable.Columns.Add("Title", typeof(String));
+                currTable.Columns.Add("Message");
+            }
+            if (archTable.Columns.Count <= 0)
+            {
+                archTable.Columns.Add("Title", typeof(String));
+                archTable.Columns.Add("Message");
+            }
+
+
+            currNotes.DataSource = currTable;
+
+            currNotes.Columns["Message"].Visible = false;
+            currNotes.Columns["Title"].Width = 289;
+            
+
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+            currNotes.DataSource = currTable;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtTitle.Text.Any() || txtMessage.Text.Any())
+            {
+                currTable.Rows.Add(txtTitle.Text, txtMessage.Text);
+                txtTitle.Clear();
+                txtMessage.Clear();
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtTitle.Clear();
+                txtMessage.Clear();
+                int index = currNotes.CurrentCell.RowIndex;
+                if (index > -1)
+                {
+                    if (currNotes.DataSource == currTable)
+                    {
+
+                        txtTitle.Text = currTable.Rows[index].ItemArray[0].ToString();
+                        txtMessage.Text = currTable.Rows[index].ItemArray[1].ToString();
+                    }
+                    else
+                    {
+                        txtTitle.Text = archTable.Rows[index].ItemArray[0].ToString();
+                        txtMessage.Text = archTable.Rows[index].ItemArray[1].ToString();
+                    }
+
+                }
+            }
+            catch { }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = currNotes.CurrentCell.RowIndex;
+                if (index > -1)
+                {
+                    if (currNotes.DataSource == currTable)
+                    {
+                        currTable.Rows[index].Delete();
+                    }
+                    else
+                    {
+                        archTable.Rows[index].Delete();
+                    }
+                    
+                }
+                txtTitle.Clear();
+                txtMessage.Clear();
+            }
+            catch
+            { }
+        }
+
+        private void btnAddArchive_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtTitle.Clear();
+                txtMessage.Clear();
+                int index = currNotes.CurrentCell.RowIndex;
+                if (index > -1)
+                {
+                    archTable.Rows.Add(currTable.Rows[index].ItemArray[0].ToString(), currTable.Rows[index].ItemArray[1].ToString());
+                    currTable.Rows[index].Delete();
+                }
+            }
+            catch { }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+            currNotes.DataSource = archTable;
+           
+            
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+            currNotes.DataSource = currTable;
+            currNotes.Columns["Message"].Visible = false;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+       
+            currSet.Tables[0].WriteXml("Current.xml");
+            archSet.Tables[0].WriteXml("Archive.xml");
+
+
+        }
+    }
+}
